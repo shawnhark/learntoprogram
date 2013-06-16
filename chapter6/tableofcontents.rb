26e80dc76d5d0d495379b2c16bb2ab0ec58c807a
Shawn Harkness <shawnhark@gmail.com>
2013-06-16 03:48:31 -0400

    Add Chapter 6 and 7 programs

---
+++ chapter6/tableofcontents.rb
@@ -0,0 +1,9 @@
+# table of contents
+width = 20
+puts ('Table of Contents'.center(width))
+puts ('Chapter 1:'.ljust(width)) + ('Getting Started'.center(width)) + ('page 1'.rjust(width))
+puts ('Chapter 2:'.ljust(width)) + ('The Basics'.center(width)) + ('page 7'.rjust(width))
+puts ('Chapter 3:'.ljust(width)) + ('Simple Tricks'.center(width)) + ('page 35'.rjust(width))
+puts ('Chapter 4:'.ljust(width)) + ('Advanced Tricks'.center(width)) + ('page 57'.rjust(width))
+puts ('Chapter 5:'.ljust(width)) + ('Using Your Knowledge'.center(width)) + ('page 93'.rjust(width))
+puts ('Index'.ljust(width)) + (' '.center(width)) + ('page 112'.rjust(width))
---
+++ chapter7/99bottles.rb
@@ -0,0 +1,11 @@
+beers = 99
+while beers > 0
+	puts beers.to_s + ' bottles of beer on the wall.'
+	puts beers.to_s + ' bottles of beer.'
+	puts 'You take one down, pass it around.'
+	beers = beers-1
+	puts beers.to_s + ' bottles of beer on the wall'
+	puts ' '
+end
+puts 'Now go home!'
+puts ' '
