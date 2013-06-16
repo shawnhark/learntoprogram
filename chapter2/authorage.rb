163fd4257023bb76ce7a9d4a00c0f5aae9c72638
Shawn Harkness <shawnhark@gmail.com>
2013-06-15 21:47:45 -0400

    Chapter 2 tests

---
+++ chapter2/authorage.rb
@@ -0,0 +1,8 @@
+puts 'age in minutes'
+puts 1111000000/60
+puts 'age in hours'
+puts 1111000000/(60*60)
+puts 'age in days'
+puts 1111000000/((60*60)*24)
+puts 'age in whole years'
+puts 1111000000/(((60*60)*24)*365)
