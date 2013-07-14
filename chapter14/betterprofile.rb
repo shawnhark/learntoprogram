a8755d54ab44c01f9086f6088307ba39efb665d2
Shawn Harkness <shawnhark@gmail.com>
2013-07-14 05:27:31 -0400

    Finish Chapter 14 exeercises

---
+++ chapter14/betterprofile.rb
@@ -0,0 +1,11 @@
+def profile block_description, &block
+  profiling_on = true
+  if profiling_on
+    start_time = Time.new
+    block.call
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+  block.call
+  end
+end
